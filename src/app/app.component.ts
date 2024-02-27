ea3aa6d7ee5ecdd1eb20103eb7e1aa1c657bdf85
Luiggi <luiggiyantas@gmail.com>
2024-02-27 13:41:08 -0500

    weather app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { Weather } from './models/weather.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'weatherApp';
+  weather?: Weather;
+  cityName = 'landon';
+
+  constructor(private weatherService: WeatherService) { }
+
+  ngOnInit(): void {
+    this.getWeather(this.cityName);
+    this.cityName = '';
+  }
+
+  convertToCelsius(tempInFahrenheit: number): number {
+    return Math.round((tempInFahrenheit - 32) * 5 / 9);
+  }
+
+  onSubmit() {
+    this.getWeather(this.cityName);
+    this.cityName = '';
+  }
+
+  private getWeather(cityName: string) {
+    this.weatherService.getWeatherData(cityName).subscribe({
+      next: res => { this.weather = res; console.log(res); },
+      error: err => console.log('Limite de consultas alcanzada', err)
+    })
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city/',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '080fd7e839msh8ad2d4ef66bd80fp177d04jsn9d971963769c',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com' 
+}
